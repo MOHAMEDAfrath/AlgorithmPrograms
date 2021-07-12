b09d505c7b772968419ef7b992f5f9a90462930e
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-12 13:59:54 +0530

    [Afrath] Updated the file with Binary Search

---
+++ AlgorithmProgram/BinarySearch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    class BinarySearch
+    {
+        public static void Binarysearch()
+        {
+            string text = File.ReadAllText(@"C:\Users\afrat\source\repos\AlgorithmProgram\AlgorithmProgram\BinarySearch.txt");
+            List<string> list = new List<string>(text.Split(" "));
+            list.Sort();
+            Console.WriteLine("Enter the word to be found");
+            string key = Console.ReadLine();
+           
+            bool result = Search(list, 0, list.Count - 1, key);
+            if (result == true)
+                Console.WriteLine("Found {0}", key);
+            else
+                Console.WriteLine("Not Found {0}", key);
+
+        }
+        public static bool Search(List<string> list,int start,int end,string key)
+        {
+            bool found = false;
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                int res = key.CompareTo(list[mid]);
+                if (res == 0)
+                    found = true;
+                if (res > 0)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }    
+            }
+           
+                
+            
+            return found;
+        }
+    }
+}
--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -8,12 +8,20 @@ namespace AlgorithmProgram
         {
             Console.WriteLine("Welcome to Algorithm Programs");
             Console.WriteLine("1) Permutaions of a string");
+            Console.WriteLine("2) Binary Search");
             switch (Console.ReadLine())
             {
                 case "1":
+                    Console.WriteLine("***Permutations of string***");
                     Permutations.GetPermutations();
                     break;
+                case "2":
+                    Console.WriteLine("***Binary Search***");
+                    BinarySearch.Binarysearch();
+                    break;
+            
             }
+
            
         }
     }
