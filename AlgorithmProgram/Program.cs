02b8b1cc5e39cf3267f3b5d3140e0864d8e32c91
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-12 10:41:37 +0530

    [Afrath] Welcome to Algorithm Program

---
+++ AlgorithmProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs");
+        }
+    }
+}
